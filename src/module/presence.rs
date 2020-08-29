a7b1ea52cb92e7513c84adc9d6d473a7844aac7a
personinblack <berkay@tuta.io>
2020-08-29 20:35:58 +0300

    Remove unnecessary imports

--- src/module/presence.rs
+++ src/module/presence.rs
@@ -1,7 +1,5 @@
-use chrono::prelude::*;
 use serenity::client::Context;
 use serenity::model::user::OnlineStatus;
-use serenity::utils::Colour;
 use serenity::model::gateway::Activity;
 
 pub fn ready(ctx: &Context) {
