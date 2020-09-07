fce671d0f720f5c520a049a52bd1644293a8854a
personinblack <berkay@tuta.io>
2020-09-07 18:30:55 +0300

    Add faq goodbot with mentions field

--- src/module/faq.rs
+++ src/module/faq.rs
@@ -7,14 +7,21 @@ lazy_static!(
         Faq {
             expected: Regex::new(r"(?i)bilen *var *m(ı|i)").unwrap(),
             outcome: String::from("bilen var mı diye sormak yerine derdini anlatman \
-                                  daha yararlı olur.")
-        }
+                                  daha yararlı olur."),
+            mentions: false
+        },
+        Faq {
+            expected: Regex::new(r"(?i)good *bot").unwrap(),
+            outcome: String::from("saol cnm <3"),
+            mentions: true,
+        },
     ];
 );
 
 struct Faq {
     expected: Regex,
     outcome: String,
+    mentions: bool,
 }
 
 pub fn message(ctx: &Context, new_message: &Message) {
@@ -23,9 +30,10 @@ pub fn message(ctx: &Context, new_message: &Message) {
     }
 
     for faq in FAQS.iter() {
-        if faq.expected.is_match(&new_message.content) {
+        if faq.expected.is_match(&new_message.content) && (!faq.mentions
+            || new_message.mentions_user(&ctx.http.get_current_user().unwrap().into()))
+        {
             new_message.reply(ctx, &faq.outcome).ok();
-            return
         }
     }
 }
