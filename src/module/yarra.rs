32e9fd2e75690100593cac5461cdbad7665c6031
personinblack <berkay@tuta.io>
2020-09-09 15:12:48 +0300

    Replace single match with if let

--- src/module/yarra.rs
+++ src/module/yarra.rs
@@ -11,7 +11,7 @@ use serenity::framework::standard::{
 #[only_in(guilds)]
 #[max_args(1)]
 pub async fn yarra(ctx: &Context, msg: &Message) -> CommandResult {
-    match msg.channel_id.send_message(&ctx, |m| {
+    if let Err(e) = msg.channel_id.send_message(&ctx, |m| {
         m.embed(|em| {
             if !msg.mentions.is_empty() {
                 em.description(&format!(
@@ -23,8 +23,8 @@ pub async fn yarra(ctx: &Context, msg: &Message) -> CommandResult {
             em.image("https://i.ibb.co/5sBvWHC/yarra.gif")
         })
     }).await {
-        Err(e) => { return Err(CommandError::from(e)) },
-        Ok(_) => ()
+        return Err(CommandError::from(e));
     };
+
     msg.delete(ctx).await.map_err(|e| { CommandError::from(e) })
 }
