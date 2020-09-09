bdf2373dabfe986dfed7a5888c3a29169352dce2
personinblack <berkay@tuta.io>
2020-09-09 06:50:46 +0300

    Implement ability of downloading images

--- src/module/addemoji.rs
+++ src/module/addemoji.rs
@@ -1,3 +1,5 @@
+use base64::encode;
+use serde_json::json;
 use serenity::model::channel::Message;
 use serenity::prelude::Context;
 use serenity::framework::standard::{
@@ -8,20 +10,52 @@ use serenity::framework::standard::{
     Delimiter,
 };
 
+static ERR_EMPTY: &str = "verdiğin linkte bi bok bulamadım.";
+static ERR_INVLINK: &str = "böyle bir link yok belki de hiç olmadı.";
+static ERR_BIG: &str = "yalnız hocam benim sınır 256kb.";
+static ERR_NOTSUP: &str = "png ve gif dışındakiler tarzım değil.";
+static ERR_SMNAME: &str = "hafız az daha uzun isim girebilicen mi.";
+
 #[command]
 #[only_in(guilds)]
 #[num_args(2)]
-pub fn addemoji(ctx: &mut Context, msg: &Message) -> CommandResult {
+pub async fn addemoji(ctx: &Context, msg: &Message) -> CommandResult {
     let mut args = Args::new(&msg.content, &[Delimiter::Single(' ')]);
     let name = match args.advance().single::<String>() {
-        Ok(name) => name,
+        Ok(name) if name.len() > 1 => name,
         Err(e) => { return Err(CommandError::from(e)) },
+        _ => { return Err(ERR_SMNAME.into()) }
     };
-    let emoji = match args.single::<String>() {
+
+    let image_url = match args.single::<String>() {
         Ok(emoji) => emoji,
         Err(e) => { return Err(CommandError::from(e)) },
     };
 
-    msg.guild(&ctx).unwrap().read().create_emoji(&ctx, &name, &emoji)
+    let image_raw = match reqwest::get(&image_url).await {
+        Ok(resp) =>  match resp.bytes().await {
+            Ok(bytes) if bytes.len() <= 255999 => bytes,
+            Err(_) => { return Err(CommandError::from(ERR_EMPTY)) }
+            _ => { return Err(ERR_BIG.into()) }
+        },
+        Err(_) => { return Err(CommandError::from(ERR_INVLINK)) }
+    };
+
+    let mut ext = String::from_utf8_lossy(&image_raw.slice(0..4).to_vec()).to_string();
+
+    println!("{:?}", image_raw);
+
+    ext = match ext.as_str() {
+        "GIF8" => "gif".into(),
+        "�PNG" => "png".into(),
+        _ => { return Err(format!("{} ~~{}~~", ERR_NOTSUP, ext).into()); }
+    };
+
+    let emoji = json!({
+        "name": name,
+        "image": format!("data:image/{};base64,{}", ext, encode(image_raw))
+    });
+
+    ctx.http.create_emoji(msg.guild_id.unwrap().into(), &emoji).await
         .map_err(|e| { CommandError::from(e) }).map(|_| ())
 }
