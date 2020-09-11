e1292b8082e68e913b60c9b4850fe1482ca67c49
personinblack <berkay@tuta.io>
2020-09-11 16:53:09 +0300

    Stop using Docker and cross-compile that shit!

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-docker run --rm -v /$(pwd)/deploy/bin:/output -v bottarget:/target -v /$(pwd):/build -v botcargo:/usr/local/cargo hwbot:latest
+cargo clippy -Z unstable-options || exit 1
+PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-musl \
+    || exit 1
+cp target/x86_64-unknown-linux-musl/release/hello_worlds-bot deploy/bin/ || exit 1
 
 cd deploy
-sudo chown menfie:menfie bin/hello_worlds-bot
-git remote add heroku https://git.heroku.com/bqwes.git
 git add --all && git commit -m "a" && git push --force heroku master
