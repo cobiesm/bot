d9f432772ab07b03fc611908256c42896fcb2176
personinblack <berkay@tuta.io>
2020-10-08 21:07:08 +0300

    Don't let mute#find_member take Args

--- src/module/mute.rs
+++ src/module/mute.rs
@@ -7,8 +7,8 @@ use serenity::{
 
 #[command]
 #[num_args(1)]
-pub async fn mute(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    find_member(ctx, msg, args)
+pub async fn mute(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    find_member(ctx, msg, &args.single::<String>()?)
         .await?
         .mute(ctx)
         .await
@@ -17,24 +17,23 @@ pub async fn mute(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 
 #[command]
 #[num_args(1)]
-pub async fn unmute(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-    find_member(ctx, msg, args)
+pub async fn unmute(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    find_member(ctx, msg, &args.single::<String>()?)
         .await?
         .unmute(ctx)
         .await
         .map_err(CommandError::from)
 }
 
-pub async fn find_member(ctx: &Context, msg: &Message, mut args: Args) -> serenity::Result<Member> {
+pub async fn find_member(ctx: &Context, msg: &Message, name: &str) -> serenity::Result<Member> {
     Ok(msg
         .guild(ctx)
         .await
         .ok_or(serenity::Error::Other("guild!!?"))?
-        .members_containing(&args.single::<String>().unwrap(), false, true)
+        .members_containing(name, false, true)
         .await
         .first()
         .ok_or(serenity::Error::Other("kim bu amk tanımıyorum."))?
         .0
         .clone())
-    //.map_or(Err(serenity::Error::Other("Kim bu amk tanımıyorum.")))
 }
