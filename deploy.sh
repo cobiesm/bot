46c5c43b4bedf58010c77abd7d04c27ad23bfeb1
personinblack <berkay@tuta.io>
2020-12-15 08:24:58 +0300

    Clear commit history while deploying

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,9 @@ PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target x86_64-unknown-linux-mus
 cp target/x86_64-unknown-linux-musl/release/hello_worlds-bot deploy/bin/ || exit 1
 
 cd deploy
-git add --all && git commit -m "a" && git push --force heroku master
+git checkout --orphan tmp
+git add --all
+git commit -m "a"
+git branch -D master
+git branch -m master
+git push --force heroku master
