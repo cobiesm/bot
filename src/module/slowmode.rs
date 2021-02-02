e5d16ebf1fbe6cfaf8a35fda0897412db9dff874
personinblack <berkay@tuta.io>
2021-02-02 23:32:52 +0300

    Make Slowmode bit more lenient

--- src/module/slowmode.rs
+++ src/module/slowmode.rs
@@ -21,7 +21,7 @@ pub async fn message(ctx: &Context, msg: &Message) {
         .collect::<Vec<Message>>();
 
     if messages.len() > 1
-        && msg.timestamp - messages.get(1).unwrap().timestamp < Duration::milliseconds(250)
+        && msg.timestamp - messages.get(1).unwrap().timestamp < Duration::milliseconds(66)
     {
         msg.delete(ctx).await.ok();
     }
